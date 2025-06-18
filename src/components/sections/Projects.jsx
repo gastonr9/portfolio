0b88c11f6821e9c0f2100b00c6c4f578a0b9c05b
Gaston Rodriguez <lpmgaston@gmail.com>
2025-06-18 00:53:46 -0300

    Refactor code structure for improved readability and maintainability

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,3 +1,4 @@
+import sublimo from "../../assets/images/sublimo-project.jpg";
 export const Projects = () => {
   return (
     <section id="proyectos" className=" items-center justify-center pt-25">
@@ -23,24 +24,28 @@ export const Projects = () => {
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div className="p-6 rounded-xl border border-black/10 hover:-translate-y-1 hover:border-blue-500/30 dark:border-white/10 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-            <h3 className="text-xl font-bold mb-2">Proximo</h3>
+            <h3 className="text-xl font-bold mb-2">Sublimo App</h3>
             <p className=" mb-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
-              harum, voluptatibus sunt dignissimos, quisquam hic at ipsa
-              necessitatibus dicta culpa placeat. Quam laudantium nostrum
-              magnam, explicabo eligendi natus magni ducimus!
+              Sublimó es una plataforma web interactiva desarrollada para
+              facilitar la visualización de diseños personalizados aplicados
+              sobre productos como remeras, buzos, gorras y llaveros. Permite a
+              usuarios visualizar en tiempo real sus creaciones sobre modelos
+              3D, personalizar colores y fondos, añadir imágenes o texto, y
+              descargar capturas del diseño como muestra visual.
             </p>
             <div className="flex flex-wrap gap-2 mb-4">
-              {["React", "TailwindCSS", "Firebase"].map((tech, key) => (
-                <span
-                  key={key}
-                  className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+              {["ReactJs", "Vite", "TailwindCSS", "TypeScript", "Firebase"].map(
+                (tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
                                     hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
                     "
-                >
-                  {tech}
-                </span>
-              ))}
+                  >
+                    {tech}
+                  </span>
+                )
+              )}
             </div>
             <div className="flex justify-between items-center">
               <a
@@ -49,6 +54,11 @@ export const Projects = () => {
               >
                 Ver Proyecto →
               </a>
+              <img
+                src={sublimo}
+                alt="sbulmio project"
+                className="rounded-xl  w-52 "
+              />
             </div>
           </div>
           <div className="p-6 rounded-xl border border-black/10 hover:-translate-y-1 hover:border-blue-500/30 dark:border-white/10 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
